a18ccd2f279fef03f0ee284abe8b1bf589c38c16
Andre <andre@dev-next.com>
2018-12-26 10:08:35 -0200

    :bug: Fixing Auth Header on API instance

--- src/utils/apiInstance.js
+++ src/utils/apiInstance.js
@@ -5,6 +5,6 @@ require('dotenv').config();
 export default axios.create({
   baseURL: 'https://api.spotify.com/v1/',
   headers: {
-    Authorization: `Bearer: ${process.env.SPOTIFY_TOKEN}`,
+    Authorization: `Bearer ${process.env.SPOTIFY_TOKEN}`,
   },
 });
